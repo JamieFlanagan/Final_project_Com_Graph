bfe76ef9ccd81a50dadcaa850d2efb8ee31e678b
JamieFlanagan <jamieflana0@gmail.com>
2024-12-26 14:13:16 +0000

    Welcome sign shadow

---
+++ cmake-build-debug/CMakeFiles/lab2_building.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/JAMIE/Documents/4thYear/ComputerGraphics/ForChecking/lab2/lab2/Final.cpp" "CMakeFiles/lab2_building.dir/lab2/Final.cpp.obj" "gcc" "CMakeFiles/lab2_building.dir/lab2/Final.cpp.obj.d"
+  "C:/Users/JAMIE/Documents/4thYear/ComputerGraphics/ForChecking/lab2/lab2/components/Billboard/Billboard.cpp" "CMakeFiles/lab2_building.dir/lab2/components/Billboard/Billboard.cpp.obj" "gcc" "CMakeFiles/lab2_building.dir/lab2/components/Billboard/Billboard.cpp.obj.d"
+  "C:/Users/JAMIE/Documents/4thYear/ComputerGraphics/ForChecking/lab2/lab2/components/Building.cpp" "CMakeFiles/lab2_building.dir/lab2/components/Building.cpp.obj" "gcc" "CMakeFiles/lab2_building.dir/lab2/components/Building.cpp.obj.d"
+  "C:/Users/JAMIE/Documents/4thYear/ComputerGraphics/ForChecking/lab2/lab2/components/CoolerParticles/movingParticles.cpp" "CMakeFiles/lab2_building.dir/lab2/components/CoolerParticles/movingParticles.cpp.obj" "gcc" "CMakeFiles/lab2_building.dir/lab2/components/CoolerParticles/movingParticles.cpp.obj.d"
+  "C:/Users/JAMIE/Documents/4thYear/ComputerGraphics/ForChecking/lab2/lab2/components/ParticleSystem/ParticleSystem.cpp" "CMakeFiles/lab2_building.dir/lab2/components/ParticleSystem/ParticleSystem.cpp.obj" "gcc" "CMakeFiles/lab2_building.dir/lab2/components/ParticleSystem/ParticleSystem.cpp.obj.d"
+  "C:/Users/JAMIE/Documents/4thYear/ComputerGraphics/ForChecking/lab2/lab2/components/WelcomeSign/WelcomeSign.cpp" "CMakeFiles/lab2_building.dir/lab2/components/WelcomeSign/WelcomeSign.cpp.obj" "gcc" "CMakeFiles/lab2_building.dir/lab2/components/WelcomeSign/WelcomeSign.cpp.obj.d"
+  "C:/Users/JAMIE/Documents/4thYear/ComputerGraphics/ForChecking/lab2/lab2/components/animation_model.cpp" "CMakeFiles/lab2_building.dir/lab2/components/animation_model.cpp.obj" "gcc" "CMakeFiles/lab2_building.dir/lab2/components/animation_model.cpp.obj.d"
+  "C:/Users/JAMIE/Documents/4thYear/ComputerGraphics/ForChecking/lab2/lab2/components/drone.cpp" "CMakeFiles/lab2_building.dir/lab2/components/drone.cpp.obj" "gcc" "CMakeFiles/lab2_building.dir/lab2/components/drone.cpp.obj.d"
+  "C:/Users/JAMIE/Documents/4thYear/ComputerGraphics/ForChecking/lab2/lab2/components/floor.cpp" "CMakeFiles/lab2_building.dir/lab2/components/floor.cpp.obj" "gcc" "CMakeFiles/lab2_building.dir/lab2/components/floor.cpp.obj.d"
+  "C:/Users/JAMIE/Documents/4thYear/ComputerGraphics/ForChecking/lab2/lab2/components/skyBox.cpp" "CMakeFiles/lab2_building.dir/lab2/components/skyBox.cpp.obj" "gcc" "CMakeFiles/lab2_building.dir/lab2/components/skyBox.cpp.obj.d"
+  "C:/Users/JAMIE/Documents/4thYear/ComputerGraphics/ForChecking/lab2/lab2/components/tinygltf_implementation.cpp" "CMakeFiles/lab2_building.dir/lab2/components/tinygltf_implementation.cpp.obj" "gcc" "CMakeFiles/lab2_building.dir/lab2/components/tinygltf_implementation.cpp.obj.d"
+  "C:/Users/JAMIE/Documents/4thYear/ComputerGraphics/ForChecking/lab2/lab2/render/shader.cpp" "CMakeFiles/lab2_building.dir/lab2/render/shader.cpp.obj" "gcc" "CMakeFiles/lab2_building.dir/lab2/render/shader.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/lab2_building.dir/cmake_clean.cmake
@@ -0,0 +1,35 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab2_building.dir/lab2/Final.cpp.obj"
+  "CMakeFiles/lab2_building.dir/lab2/Final.cpp.obj.d"
+  "CMakeFiles/lab2_building.dir/lab2/components/Billboard/Billboard.cpp.obj"
+  "CMakeFiles/lab2_building.dir/lab2/components/Billboard/Billboard.cpp.obj.d"
+  "CMakeFiles/lab2_building.dir/lab2/components/Building.cpp.obj"
+  "CMakeFiles/lab2_building.dir/lab2/components/Building.cpp.obj.d"
+  "CMakeFiles/lab2_building.dir/lab2/components/CoolerParticles/movingParticles.cpp.obj"
+  "CMakeFiles/lab2_building.dir/lab2/components/CoolerParticles/movingParticles.cpp.obj.d"
+  "CMakeFiles/lab2_building.dir/lab2/components/ParticleSystem/ParticleSystem.cpp.obj"
+  "CMakeFiles/lab2_building.dir/lab2/components/ParticleSystem/ParticleSystem.cpp.obj.d"
+  "CMakeFiles/lab2_building.dir/lab2/components/WelcomeSign/WelcomeSign.cpp.obj"
+  "CMakeFiles/lab2_building.dir/lab2/components/WelcomeSign/WelcomeSign.cpp.obj.d"
+  "CMakeFiles/lab2_building.dir/lab2/components/animation_model.cpp.obj"
+  "CMakeFiles/lab2_building.dir/lab2/components/animation_model.cpp.obj.d"
+  "CMakeFiles/lab2_building.dir/lab2/components/drone.cpp.obj"
+  "CMakeFiles/lab2_building.dir/lab2/components/drone.cpp.obj.d"
+  "CMakeFiles/lab2_building.dir/lab2/components/floor.cpp.obj"
+  "CMakeFiles/lab2_building.dir/lab2/components/floor.cpp.obj.d"
+  "CMakeFiles/lab2_building.dir/lab2/components/skyBox.cpp.obj"
+  "CMakeFiles/lab2_building.dir/lab2/components/skyBox.cpp.obj.d"
+  "CMakeFiles/lab2_building.dir/lab2/components/tinygltf_implementation.cpp.obj"
+  "CMakeFiles/lab2_building.dir/lab2/components/tinygltf_implementation.cpp.obj.d"
+  "CMakeFiles/lab2_building.dir/lab2/render/shader.cpp.obj"
+  "CMakeFiles/lab2_building.dir/lab2/render/shader.cpp.obj.d"
+  "lab2_building.exe"
+  "lab2_building.exe.manifest"
+  "lab2_building.pdb"
+  "liblab2_building.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab2_building.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
